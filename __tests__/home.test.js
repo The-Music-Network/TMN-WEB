360bee498f25ed905ec04ab2b85ad0bf6440acc6
theRealScoobaSteve <stephensimone@live.com>
2018-09-20 17:30:49 -0700

    Got mocha prepare working and added test tests

--- __tests__/home.test.js
+++
@@ -1,5 +0,0 @@
-describe("Testing the home page", function () {
-    it("should load", function () {
-        console.log(kermit)
-    });
-});
--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    globalSetup: "./jest/setup.js",
-    globalTeardown: "./jest/teardown.js",
-    testEnvironment: "./jest/kermit_environment.js"
-};
