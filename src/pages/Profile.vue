4c543562373b3f09ac967f37e0d3af65a1605cb9
theRealScoobaSteve <stephensimone@live.com>
2018-09-25 19:07:52 -0700

    Added col-x to main.scss

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,8 +1,21 @@
 <template>
-   <div>
-    Profile
+   <div class="tmn-container">
+    <div class="profile--main-pic row">
+        <div class="col-3 center-text">
+            Hello
+        </div>
+        <div class="col-9 center-text">
+            World
+        </div>
+    </div>
    </div>
 </template>
 <script>
 export default {};
 </script>
+<style lang="scss">
+@import "styles/index.scss";
+
+.profile--main-pic {
+}
+</style>
