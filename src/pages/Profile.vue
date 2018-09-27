f77398219f27d8892f642cda90b5eaa5bd0f7e15
theRealScoobaSteve <stephensimone@live.com>
2018-09-26 21:23:03 -0700

    Added banner

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -8,18 +8,26 @@
                  </div>
             </div>
         </div>
-        <div class="col-6 text-center">
-            Test
-        </div>
-        <div class="col">
-            Test
+        <div class="col-9">
+            <div class="row">
+                <div class="profile--banner">
+                    <img src="https://www.bristolgate.com/wp-content/uploads/2018/09/orionthemes-placeholder-image.png">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-9 text-center">
+                    Feed
+                </div>
+                <div class="col-3 text-center">
+                    Suggestions
+                </div>
+            </div>
         </div>
     </div>
    </div>
 </template>
 <script>
-export default {
-};
+export default {};
 </script>
 <style lang="scss">
 @import "styles/index.scss";
@@ -30,14 +38,21 @@ export default {
     height: 93vh;
   }
 
-  &--img {
-    img {
-        height: 150px;
-        width: 150px;
-        display: block;
-        margin: 0 auto;
-        padding: 20px;
-        border-radius: 200px;
+  &--img img {
+    height: 200px;
+    width: 200px;
+    display: block;
+    margin: 0 auto;
+    padding: 20px;
+    border-radius: 200px;
+    object-fit: cover;
+  }
+
+  &--banner {
+    & img {
+      height: 25vh;
+      width: 100%;
+      object-fit: cover;
     }
   }
 }
