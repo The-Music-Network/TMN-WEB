25cf42699774e7d9654bdec5bef3cbfefc66ae29
theRealScoobaSteve <stephensimone@live.com>
2018-09-26 20:43:03 -0700

    Added beggining of profile page

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,26 +1,44 @@
 <template>
-   <div>
+   <div class="container">
     <div class="row">
-        <div class="col-6 center-text">
+        <div class="col">
             <div class="profile--left">
-                 Make stuff happen
+                 <div class="profile--img">
+                    <img src="https://www.bristolgate.com/wp-content/uploads/2018/09/orionthemes-placeholder-image.png">
+                 </div>
             </div>
         </div>
-        <div class="col-6 center-text">
+        <div class="col-6 text-center">
+            Test
+        </div>
+        <div class="col">
             Test
         </div>
     </div>
    </div>
 </template>
 <script>
-export default {};
+export default {
+};
 </script>
 <style lang="scss">
 @import "styles/index.scss";
 
 .profile {
   &--left {
-    background-color: $secondary-color;
+    background-color: $light-secondary-color;
+    height: 93vh;
+  }
+
+  &--img {
+    img {
+        height: 150px;
+        width: 150px;
+        display: block;
+        margin: 0 auto;
+        padding: 20px;
+        border-radius: 200px;
+    }
   }
 }
 </style>
