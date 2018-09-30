ff33d28e68de29eb80b9cfc1deefbb4d79d7741d
theRealScoobaSteve <stephensimone@live.com>
2018-09-30 14:14:46 -0700

    Fixing elegant nav

---
+++ src/components/ElegantLink.vue
@@ -0,0 +1,17 @@
+<template>
+    <a @click="setActive"><slot></slot></a>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isActive: false
+    };
+  },
+  methods: {
+    setActive() {
+      this.isActive = !this.isActive;
+    }
+  }
+};
+</script>
---
+++ src/components/ElegantNav.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="nav nav--elegant">
+        <nav>
+            <elegant-link>Music</elegant-link>
+            <elegant-link>Playlists</elegant-link>
+            <elegant-link>Followers</elegant-link>
+            <elegant-link>Following</elegant-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+import ElegantLink from "./ElegantLink.vue";
+export default {
+  components: {
+    ElegantLink
+  }
+};
+</script>
+<style lang="scss">
+</style>
